a9cfeffea936df06d583811705ed30b9a4bfef07
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-12-07 14:27:09 -0300

    Iniciado o projeto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// start project frontend
+// start project frontend now
 
 ReactDOM.render(
   <React.StrictMode>
